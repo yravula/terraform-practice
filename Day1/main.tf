0c0ce444b2d935e18b691d2939a1e0909977f493
yravula <yravula@gmail.com>
2024-10-23 22:55:05 +0530

    first code

---
+++ Day1/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"  
+}
+
+resource "aws_instance" "web" {
+  ami           = ami-09c813fb71547fc4f
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "web"
+  }
+}
